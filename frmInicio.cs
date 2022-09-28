6aa397708797b7e1aeb8c19a6e3311041ba2a062
root <mgiraudo@ies21.edu.ar>
2022-09-28 08:16:52 -0300

    cierre formulario

--- frmInicio.cs
+++ frmInicio.cs
@@ -17,7 +17,7 @@ namespace pryArmaniniSP1_N1
             InitializeComponent();
         }
 
-        int intentos = 3;
+        int intentos = 0;
         private void btnAceptar_Click(object sender, EventArgs e)
         {
             if ((txtUsuario1.Text == "Administrador" &&
@@ -38,10 +38,10 @@ namespace pryArmaniniSP1_N1
             {
                 MessageBox.Show("Datos Incorrectos, Acceso Denegado.");
                 intentos++; // incrementa el contador de intentos fallidos
-                if (intentos == 3) // si es 3 se cierra el formulario
-                {
-                    this.Close();
-                }
+            }
+            if (intentos == 3) // si es 3 se cierra el formulario
+            {
+                this.Close();
             }
 
         }
